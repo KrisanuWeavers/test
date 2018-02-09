2ffc8a8da2c1d45757efc661a09b0184d18813c3
unknown <rahul.weavers@gmail.com>
2018-02-09 13:06:43 +0530

    Putting back button in setting,edit profile, change password page and bug fix in favourites page.

--- app/main/controllers/setting-ctrl.js
+++ app/main/controllers/setting-ctrl.js
@@ -1,7 +1,9 @@
 'use strict';
 angular.module('main')
-.controller('SettingCtrl', function ($log) {
-
-  $log.log('Hello from your Controller: SettingCtrl in module main:. This is your controller:', this);
-
-});
+  .controller('SettingCtrl', function ($log, $state) {
+    var ctrl = this;
+    $log.log('Hello from your Controller: SettingCtrl in module main:. This is your controller:', this);
+    ctrl.goBack = function () {
+      $state.go('main.profile');
+    };
+  });
