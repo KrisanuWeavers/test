60236f89cba67f7e5e6e3c0d4c68c6d1c2f5b35f
unknown <rahul.weavers@gmail.com>
2018-02-12 20:36:44 +0530

    Venu list and details integration from google place api.

--- app/main/controllers/event-details-ctrl.js
+++ app/main/controllers/event-details-ctrl.js
@@ -1,8 +1,32 @@
 'use strict';
 angular.module('main')
-  .controller('EventDetailsCtrl', function ($log, $scope, $ionicScrollDelegate, $window, $state, $ionicModal) {
+  .controller('EventDetailsCtrl', function ($log, $scope, $ionicScrollDelegate, $window, $state, $ionicModal, Store, $ionicLoading) {
     var ctrl = this;
     $log.log('Hello from your Controller: EventDetailsCtrl in module main:. This is your controller:', this);
+    $log.log($state.params.placeid);
+    ctrl.eventMainImgRef = '';
+    ctrl.eventMainImgRef = '';
+    $ionicLoading.show({
+      content: 'Loading',
+      animation: 'fade-in',
+      showBackdrop: true,
+      maxWidth: 200,
+      showDelay: 0
+    });
+    $scope.$on('$ionicView.afterEnter', function () {
+      Store.getListDetails($state.params.placeid)
+        .then(function (response) {
+          $ionicLoading.hide();
+          $log.log('======Event Details=======');
+          $log.log(response);
+          ctrl.eventMainImgRef = response.data.result.photos[0].photo_reference;
+          ctrl.eventMainImg = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=' + ctrl.eventMainImgRef + '&key=AIzaSyAf466T-Gg01djruv0MXZ4-9ws8MapLhK8';
+        }).catch(function (error) {
+          //ctrl.isAnyLatest = false;
+          $ionicLoading.hide();
+          $log.log(error);
+        });
+    });
     /**********************************
      * Change header bar background transparent to color alpha when scrolling
      * https://codepen.io/bennyboybrizzle/pen/ByvrMN
